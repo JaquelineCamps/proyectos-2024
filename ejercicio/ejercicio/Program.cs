4c11470d06b778ede32133c770c1f4de1f816984
Jaqueline Campos <TSNP210223@ugb.edu.sv>
2024-07-24 11:26:26 -0600

    ejemplos

---
+++ ejercicio/ejercicio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int suma = 0;
+
+            do
+            {
+                Console.Write("Introduce un número positivo: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero > 0)
+                {
+                    suma += numero;
+                }
+                else
+                {
+                    break;
+                }
+
+            } while (true);
+
+            Console.WriteLine("La suma de los números positivos introducidos es: " + suma);
+        }
+    }
+}
