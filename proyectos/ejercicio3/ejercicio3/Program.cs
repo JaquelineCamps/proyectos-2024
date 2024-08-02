7e2aee512f788eab0bce39cd348958576eebea5b
Jaqueline Campos <TSNP210223@ugb.edu.sv>
2024-08-02 10:23:04 -0600

    bloque1 ejercicio3

---
+++ proyectos/ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int resultado;
+
+            do
+            {
+
+                Console.Write("Escriba un numero positivo;(negativo o cero es para finalizar):");
+                numero1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Escriba otro numero positivo,(negativo o cero es para finalizar):");
+                numero2 = Convert.ToInt32(Console.ReadLine());
+                resultado = numero1 + numero2;
+                if (numero1 != 0 || numero2 != 0)
+
+
+                    Console.WriteLine("El resultado es: {0}", resultado);
+                break;
+
+            } while (numero1 != 0 || numero2 != 0);
+
+
+            Console.Write(" terminara cuando escriba un numero negativo o cero");
+            Console.ReadKey();
+        }
+    }
+}
